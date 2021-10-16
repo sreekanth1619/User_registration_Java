51271f9cbf0b4e6c094f3f4a5ea4772e93d07aa2
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-16 16:54:29 +0530

    Added: user need to enter valid email adress

--- userregistration/src/test/java/com/bridgelabs/userregistration/UserRegistrationTest.java
+++ userregistration/src/test/java/com/bridgelabs/userregistration/UserRegistrationTest.java
@@ -8,35 +8,55 @@ import org.junit.Test;
  */
 public class UserRegistrationTest {
 	 UserRegistration userRegistration;
+
 	    @Before
 	    public void before() {
 	        userRegistration = new UserRegistration();
 	    }
 
+	    /**
+	     * Unit test for validating first name
+	     */
 	    @Test
 	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
-	        boolean result = userRegistration.firstName("sreekanth");
+	        boolean result = userRegistration.firstName("Utkarsh");
 	        Assert.assertEquals(true, result);
 	    }
 
 	    @Test
 	    public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
-	        boolean result = userRegistration.firstName("sreekanth");
+	        boolean result = userRegistration.firstName("utkarsh");
 	        Assert.assertEquals(false, result);
 	    }
+
 	    /**
 	     * Unit test for validating last name
 	     */
 	    @Test
 	    public void givenLastName_WhenProper_ShouldReturnTrue() {
-	        boolean result = userRegistration.lastName("sree");
+	        boolean result = userRegistration.lastName("Mishra");
 	        Assert.assertEquals(true, result);
 	    }
 
 	    @Test
 	    public void givenLastName_WhenNotProper_ShouldReturnFalse() {
-	        boolean result = userRegistration.lastName("sree");
+	        boolean result = userRegistration.lastName("mishra");
 	        Assert.assertEquals(false, result);
 
+	    }
+
+	    /**
+	     * Unit test for validating email address
+	     */
+	    @Test
+	    public void givenEmail_WhenProper_ShouldReturnTrue() {
+	        boolean result = userRegistration.email("abc@gmail.com.com");
+	        Assert.assertEquals(true, result);
+	    }
+
+	    @Test
+	    public void givenEmail_WhenNotProper_ShouldReturnFalse() {
+	        boolean result = userRegistration.email("abc()*@gmail.com");
+	        Assert.assertEquals(false, result);
 	    }
 }
