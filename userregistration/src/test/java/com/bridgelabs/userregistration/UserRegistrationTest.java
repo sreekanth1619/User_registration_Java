3854cf7254138a111c6340487df96f23cf718616
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-16 16:34:49 +0530

    Added: user need to valid last name

--- userregistration/src/test/java/com/bridgelabs/userregistration/UserRegistrationTest.java
+++ userregistration/src/test/java/com/bridgelabs/userregistration/UserRegistrationTest.java
@@ -24,4 +24,19 @@ public class UserRegistrationTest {
 	        boolean result = userRegistration.firstName("utkarsh");
 	        Assert.assertEquals(false, result);
 	    }
+	    /**
+	     * Unit test for validating last name
+	     */
+	    @Test
+	    public void givenLastName_WhenProper_ShouldReturnTrue() {
+	        boolean result = userRegistration.lastName("Mishra");
+	        Assert.assertEquals(true, result);
+	    }
+
+	    @Test
+	    public void givenLastName_WhenNotProper_ShouldReturnFalse() {
+	        boolean result = userRegistration.lastName("mishra");
+	        Assert.assertEquals(false, result);
+
+	    }
 }
